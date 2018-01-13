38b35fdc41a4bfa39199af4dc31a5920d8bb43a9
yaoguais <newtopstdio@163.com>
2018-01-13 13:46:18 +0800

    remove cast

--- sober_test.go
+++ sober_test.go
@@ -1,6 +1,7 @@
 package sober
 
 import (
+	"github.com/spf13/cast"
 	"github.com/stretchr/testify/assert"
 	"github.com/yaoguais/sober/ds"
 	"testing"
@@ -12,25 +13,24 @@ func init() {
 
 func TestSober(t *testing.T) {
 	assert.Equal(t, "yaoguai", String("user.name"))
-	assert.Equal(t, true, Bool("key"))
+	assert.Equal(t, false, cast.ToBool(String("key")))
 	const age = 25
 	var v0 int = age
-	assert.Equal(t, v0, Int("user.age"))
+	assert.Equal(t, v0, cast.ToInt(String("user.age")))
 	var v1 int32 = age
-	assert.Equal(t, v1, Int32("user.age"))
+	assert.Equal(t, v1, cast.ToInt32(String("user.age")))
 	var v2 int64 = age
-	assert.Equal(t, v2, Int64("user.age"))
+	assert.Equal(t, v2, cast.ToInt64(String("user.age")))
 	var v3 uint = age
-	assert.Equal(t, v3, Uint("user.age"))
+	assert.Equal(t, v3, cast.ToUint(String("user.age")))
 	var v4 uint32 = age
-	assert.Equal(t, v4, Uint32("user.age"))
+	assert.Equal(t, v4, cast.ToUint32(String("user.age")))
 	var v5 uint64 = age
-	assert.Equal(t, v5, Uint64("user.age"))
+	assert.Equal(t, v5, cast.ToUint64(String("user.age")))
 	var v6 float32 = 0.000001
-	assert.Equal(t, true, v6-Float32("float") == 0)
+	assert.Equal(t, true, v6-cast.ToFloat32(String("float")) == 0)
 	var v7 float64 = 0.000001
-	assert.Equal(t, true, v7-Float64("float") == 0)
-	assert.Nil(t, LastError())
+	assert.Equal(t, true, v7-cast.ToFloat64(String("float")) == 0)
 }
 
 func BenchmarkSober(b *testing.B) {
